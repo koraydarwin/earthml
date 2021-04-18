04a28ffc63bff13729e9839f22adb573d072848e
koraydarwin <66494389+koraydarwin@users.noreply.github.com>
2021-04-18 16:19:18 +0300

    Update noise_aug-2.py

--- package/core/noise_aug-2.py
+++ package/core/noise_aug-2.py
@@ -64,19 +64,20 @@ def noise_aug(noise_type,
 
 
 
-def expo_add_noise(data):
+        
+def gauss_add_noise(data):
             
-    'Randomly add exponential noise onto waveforms.'
+    'Randomly add Gaussian noise onto waveforms.'
             
     data_noisy = np.empty((data.shape))
     if np.random.uniform(0, rate_thres) < rate and all(snr >= snr_thres): 
         data_noisy = np.empty((data.shape))
-        data_noisy[:, 0] = data[:,0] * (1 + np.random.exponential(scale_params["scale_expo"], data.shape[0]))
-        data_noisy[:, 1] = data[:,1] * (1 + np.random.exponential(scale_params["scale_expo"], data.shape[0]))
-        data_noisy[:, 2] = data[:,2] * (1 + np.random.exponential(scale_params["scale_expo"], data.shape[0])) 
+        data_noisy[:, 0] = data[:,0] + np.random.normal(0, np.random.uniform(scale_params["low_bound_gauss"], scale_params["up_bound_gauss"])*max(data[:,0]), data.shape[0])
+        data_noisy[:, 1] = data[:,1] + np.random.normal(0, np.random.uniform(scale_params["low_bound_gauss"], scale_params["up_bound_gauss"])*max(data[:,1]), data.shape[0])
+        data_noisy[:, 2] = data[:,2] + np.random.normal(0, np.random.uniform(scale_params["low_bound_gauss"], scale_params["up_bound_gauss"])*max(data[:,2]), data.shape[0])    
     else:
         data_noisy = data
-            
+                
     return data_noisy 
 
 
