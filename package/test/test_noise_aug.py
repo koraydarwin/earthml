3dc4be07a082ffbdfcf4f65c3d4c2c26fe3bf464
koraydarwin <66494389+koraydarwin@users.noreply.github.com>
2021-04-18 17:02:56 +0300

    Create test_noise_aug.py

---
+++ package/test/test_noise_aug.py
@@ -0,0 +1,16 @@
+def test_noise_aug():
+    
+    shape = data.shape[0]
+    augmented_shape = noise_aug(noise_type,
+                                data,
+                                snr,
+                                rate,
+                                snr_thres = 10,
+                                rate_thres = 1,
+                                scale_params = {
+                                "low_bound_gauss": 0.01, 
+                                "up_bound_gauss": 0.15, 
+                                "scale_expo": 4})[0]
+    
+    
+    assert shape == augmented_shape "shape error"
