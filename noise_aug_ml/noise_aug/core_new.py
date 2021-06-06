a1cef5a6fc48723b89b489ad3e50255a1ce20dad
koraydarwin <66494389+koraydarwin@users.noreply.github.com>
2021-06-06 10:43:36 +0300

    Update core_new.py

--- noise_aug_ml/noise_aug/core_new.py
+++ noise_aug_ml/noise_aug/core_new.py
@@ -24,11 +24,11 @@ NOISE_TYPE2DEFAULT_PARAMS = {
     "multiplicative_rayleigh"     : MULTIPLICATIVE_NOISE_PARAMS}
 
 def noise_aug(data,
-               noise_type,
-               snr,
-               rate,
-               scale_params = None):
-     '''Return a noisy version of data for a fraction (`rate`) of the data
+              noise_type,
+              snr,
+              rate,
+              scale_params = None):
+    '''Return a noisy version of data for a fraction (`rate`) of the data
      samples if snr > scale_params['snr_threshold']. For the remaining
      fraction, return original data unchanged. Also return original
      data if snr is lower than the threshold.
@@ -56,20 +56,20 @@ def noise_aug(data,
         XXX TODO snr_thres is not really a "scale parameter"
      Returns:
      NumPy array with the shape of the original data argument.
-     '''
+     '''   
     assert noise_type in NOISE_TYPES
     if scale_params is None:
         scale_params = NOISE_TYPE2DEFAULT_PARAMS[noise_type]
         _check_options(noise_type, scale_params)
-
-    if np.random.uniform(0, 1) < rate and snr >= scale_params["snr_thres"]: 
+        
+    if np.random.uniform(0, 1) < rate and snr >= scale_params["snr_thres"]:
         noise_func = NOISE_TYPE2FUNC[noise_type]
         noise_augmented = noise_func(data = data, scale_params = scale_params)
     else:
         noise_augmented = data
     return noise_augmented
 
- def _check_options(noise_type, scale_params_main):
+def _check_options(noise_type, scale_params_main):
     assert noise_type in NOISE_TYPES
     if noise_type == "additive_gaussian" or noise_type == "additive_exponential":
         if "scale" in scale_params_main:
@@ -80,7 +80,7 @@ def noise_aug(data,
              "%s does not use low_bound_scale or up_bound_scale" % noise_type)
         
 def gauss_add_noise(data, scale_params):
-     '''Apply additive Gaussian noise with a random scale variable onto 
+    '''Apply additive Gaussian noise with a random scale variable onto 
      raw waveform data'''
     data_noisy = np.empty(data.shape)
     num_samples, num_channels = data.shape
@@ -90,7 +90,7 @@ def gauss_add_noise(data, scale_params):
              scale_params["up_bound_scale"])
         noise = np.random.normal(0, channel_scale * max(data[:,i]), num_samples)
         data_noisy[:, i] = data[:,i] + noise
-    return data_noisy 
+    return data_noisy
 
 def exp_add_noise(data, scale_params):
     '''Apply additive exponential noise with a random scale variable onto
@@ -104,26 +104,28 @@ def exp_add_noise(data, scale_params):
            scale_params["up_bound_scale"])
         noise = signs * np.random.exponential(channel_scale * max(data[:,i]), num_samples)
         data_noisy = data + noise
-    return data_noisy   
+    return data_noisy
 
 def exp_mult_noise(data, scale_params):
-     '''Apply multiplicative exponential noise with a scale value determined by
+    '''Apply multiplicative exponential noise with a scale value determined by
      scale_params['scale'] onto raw waveform data'''
-        data_noisy = np.empty(data.shape)
-        noise_fac = 1 + np.random.exponential(scale_params['scale'], data.shape)
-        data_noisy = data * noise_fac
-    return data_noisy   
-
-def ray_mult_noise(data, scale_params):
-     '''Apply multiplicative Rayleigh noise with a scale value determined by
-     scale_params['scale'] onto raw waveform data'''
-        data_noisy = np.empty(data.shape)
-        noise_fac = 1 + np.random.rayleigh(scale_params['scale'], data.shape)
-        data_noisy = data * noise_fac
+    data_noisy = np.empty(data.shape)
+    noise_fac = 1 + np.random.exponential(scale_params['scale'], data.shape)
+    data_noisy = data * noise_fac
     return data_noisy
+
+def ray_mult_noise(data, scale_params):  
+    '''Apply multiplicative Rayleigh noise with a scale value determined by
+     scale_params['scale'] onto raw waveform data'''
+    data_noisy = np.empty(data.shape)
+    noise_fac = 1 + np.random.rayleigh(scale_params['scale'], data.shape)
+    data_noisy = data * noise_fac
+    return data_noisy
+
 
 NOISE_TYPE2FUNC = {
     "additive_gaussian"          : gauss_add_noise,
     "additive_exponential"       : exp_add_noise,
     "multiplicative_exponential" : exp_mult_noise,
-    "multiplicative_rayleigh"    : ray_mult_noise}
+    "multiplicative_rayleigh"    : ray_mult_noise}    
+    
