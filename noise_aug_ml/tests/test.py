d4cfeceb4f5bba43240812ab7d67c3d1ced57412
koraydarwin <66494389+koraydarwin@users.noreply.github.com>
2021-08-12 19:45:43 +0300

    Update test.py

--- noise_aug_ml/tests/test.py
+++ noise_aug_ml/tests/test.py
@@ -1,113 +1,63 @@
 import numpy as np
 import pytest
-from noise_aug.core import noise_aug
+from noise_aug import noise_aug
 
-data = np.array([[2,3,4], [5,6,7], [8,9,10]])
-raw_data_shape = data.shape
+RAW_DATA = np.array([[ 2,  3,  4,  5],
+                     [ 6,  7,  8,  9],
+                     [10, 11, 12, 13]])
 
+ADDITIVE_SCALE_PARAMS = {
+    "low_bound_scale" : 0.01,
+    "up_bound_scale"  : 0.15,
+    "snr_thres"       : 10}
 
-def test_dimension_add_gauss():
-    """ Checks whether the dimension of the original data is equal to the augmented data. """
-    noisy_data_shape = noise_aug("additive_gaussian",
-                                data,
-                                11,
-                                2,
-                                {"low_bound_gauss": 0.01, "up_bound_gauss": 0.15, "snr_thres":10, "rate_thres":1}).shape
-    assert raw_data_shape == noisy_data_shape
+MULTIPLICATIVE_SCALE_PARAMS = {
+    "scale"     : 4, 
+    "snr_thres" : 10}
 
-def test_dimension_mult_exp():
-    """ Checks whether the dimension of the original data is equal to the augmented data. """
-    noisy_data_shape = noise_aug("multiplicative_exponential",
-                                data,
-                                11,
-                                2,
-                                {"scale": 4, "snr_thres":10, "rate_thres":1}).shape
-    assert raw_data_shape == noisy_data_shape
-
-def test_dimension_mult_ray():
-    """ Checks whether the dimension of the original data is equal to the augmented data. """
-    noisy_data_shape = noise_aug("multiplicative_rayleigh",
-                                data,
-                                11,
-                                2,
-                                {"scale": 4, "snr_thres":10, "rate_thres":1}).shape
-    assert raw_data_shape == noisy_data_shape
-
-def test_dimension_add_exp():
-    """ Checks whether the dimension of the original data is equal to the augmented data. """
-    noisy_data_shape = noise_aug("additive_exponential",
-                                data,
-                                11,
-                                2,
-                                {"scale": 4, "snr_thres":10, "rate_thres":1}).shape
-    assert raw_data_shape == noisy_data_shape                       
+ADDITIVE_ARGS = {
+    'snr'         : 11,
+    'rate'        : 1,
+    'scale_params': ADDITIVE_SCALE_PARAMS}
     
-def test_add_gaus():
-    noisy_data = noise_aug("additive_gaussian",
-              data,
-              11,
-              2,
-              {"low_bound_gauss": 0.01, "up_bound_gauss": 0.15, "snr_thres":10, "rate_thres":1})
-    assert not(data == noisy_data).all()
-        
-def test_mult_exp():
-    noisy_data = noise_aug("multiplicative_exponential",
-              data,
-              11,
-              2,
-              {"scale": 4, "snr_thres":10, "rate_thres":1})
-    assert not(data == noisy_data).all()
-    
-def test_mult_ray():
-    noisy_data = noise_aug("multiplicative_rayleigh",
-              data,
-              11,
-              2,
-              {"scale": 4, "snr_thres":10, "rate_thres":1})
-    assert not(data == noisy_data).all()
+MULTIPLICATIVE_ARGS = {
+    'snr'         : 11,
+    'rate'        : 1,
+    'scale_params': MULTIPLICATIVE_SCALE_PARAMS}
 
-def test_add_exp():
-    noisy_data = noise_aug("additive_exponential",
-              data,
-              11,
-              2,
-              {"scale": 4, "snr_thres":10, "rate_thres":1})
-    assert not(data == noisy_data).all()
+NOISE_TYPE2ARGS = {
+    "additive_gaussian"         : ADDITIVE_ARGS,
+    "additive_exponential"      : ADDITIVE_ARGS,
+    "multiplicative_exponential": MULTIPLICATIVE_ARGS,
+    "multiplicative_rayleigh"   : MULTIPLICATIVE_ARGS}
 
-def check_noise(noise_type):
-    """Checks whether the given noise_type is valid or not by looking its dimension. If noise_type is invalid,
-                                                                                     main function will not return data, data will not have shape."""
-    flag = None
-    if noise_type == "additive_gaussian":
-        noisy_data_shape = noise_aug("additive_gaussian", data, 11, 3).shape
-        if noisy_data_shape == raw_data_shape:
-            flag = True
-    elif noise_type == "multiplicative_exponential":
-        noisy_data_shape = noise_aug("multiplicative_exponential", data, 11, 3).shape
-        if noisy_data_shape == raw_data_shape:
-            flag = True
-    elif noise_type == "multiplicative_rayleigh":
-        noisy_data_shape = noise_aug("multiplicative_rayleigh", data, 11, 3).shape
-        if noisy_data_shape == raw_data_shape:
-            flag = True
-    elif noise_type == "additive_exponential":
-        noisy_data_shape = noise_aug("additive_exponential", data, 11, 3).shape
-        if noisy_data_shape == raw_data_shape:
-            flag = True
-    else:
-        flag = False
-        raise ValueError("invalid noise_type") 
-    return flag
+def test_dimensions():
+    '''check dimensions of noisy data'''
+    for noise_type, kwargs in NOISE_TYPE2ARGS.items():
+        noisy_data = noise_aug(RAW_DATA, noise_type, **kwargs)
+        assert RAW_DATA.shape == noisy_data.shape
 
-def test_add_gaus1():
-    assert check_noise("additive_gaussian")
+def test_data_changed():
+    '''make sure all data is modified when rate==1'''
+    # XXX TODO this is a risky test! randomness means we will
+    # sometimes (rarely) have unmodified data despite the code working
+    # properly
+    for noise_type, kwargs in NOISE_TYPE2ARGS.items():
+        noisy_data = noise_aug(RAW_DATA, noise_type, **kwargs)
+        assert (RAW_DATA != noisy_data).all()
 
-def test_mult_exp1():
-    assert check_noise("multiplicative_exponential")
-    
-def test_mult_ray1():
-    assert check_noise("multiplicative_rayleigh")
-    
-def test_add_exp1():
-    assert check_noise("additive_exponential")
-    
+def test_invalid_noise_type():
+    '''make sure we get an error when we use an undefined error type'''
+    kwargs = NOISE_TYPE2ARGS['additive_exponential']
+    with pytest.raises(AssertionError):
+        noisy_data = noise_aug(RAW_DATA, 'junk_noise', **kwargs)
+
+MULTIPLICATIVE_NOISE_TYPE2ARGS = {k: v for k, v in NOISE_TYPE2ARGS.items() if
+                            k != 'additive_gaussian' or k != 'additive_exponential'}
+def test_invalid_scale_args():
+    '''make sure we get an error when we use additive noise arguments with multiplicative
+    noise type'''
+    gauss_kwargs = NOISE_TYPE2ARGS['additive_gaussian']
+    for noise_type in MULTIPLICATIVE_NOISE_TYPE2ARGS:
+        with pytest.raises(ValueError):
+            noisy_data = noise_aug(RAW_DATA, noise_type, **gauss_kwargs)
